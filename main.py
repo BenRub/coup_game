d1a4758d9becc667f615276d67ead58e03e66f8e
BenRub <benrub@gmail.com>
2020-09-19 23:12:26 +0300

    Redirect to index after login

--- main.py
+++ main.py
@@ -56,6 +56,7 @@ def create_app(test_config=None):
             try:
                 player = game.RegisterPlayer(request.form['username'])
                 session['playerId'] = player.id
+                return redirect(url_for('index'))
             except CoupException as e:
                 return e.message, 401
 
